3a4b6629a7a36e771917249e18ae2eb7fab06128
Pavel Khokhlov <ya.pavel-kh@yandex.ru>
2021-05-05 23:21:11 +0300

    feat: create saved-movies page

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { withRouter } from "react-router-dom";
 import Button from "../../components/Button/Button";
 
 import "./Card.css";
 
-const Card = ({ card }) => {
+const Card = ({ card, location }) => {
+  const [currentPath, setCurrentPath] = useState(location.pathname);
+
+  useEffect(() => {
+    const { pathname } = location;
+    setCurrentPath(pathname);
+  }, [location]);
+
   const buttonMovieClassName = `button button__movie ${card.like === true ? "button__movie_saved" : "button__movie_unsaved"}`;
 
   return (
@@ -15,11 +23,12 @@ const Card = ({ card }) => {
             {card.duration}
           </p>
         </div>
-        <Button type="button" className={buttonMovieClassName}></Button>
+        {currentPath === "/movies" && <Button type="button" className={buttonMovieClassName}></Button>}
+        {currentPath === "/saved-movies" && <Button type="button" className="button button__movie button__movie_delete"></Button>}
       </div>
       <img src={card.link} alt={card.title} className="movie__image" />
     </li>
   );
 };
 
-export default Card;
+export default withRouter(Card);
--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React from "react";
+import SearchForm from "../SearchForm/SearchForm";
+import Button from "../Button/Button";
+import Card from "../Card/Card";
+import movies from "../../utils/movies";
 
-import './SavedMovies.css';
+import "./SavedMovies.css";
 
 const SavedMovies = () => {
+  const myMovies = movies.filter(i => i.like === true);
+  console.log(myMovies);
+
   return (
-    <section className="Savedmovies">
-      <h2>SavedMovies</h2>
+    <section className="section">
+      <SearchForm />
+      <line className="line line__color_grey" />
+      {/* MOVIES */}
+      <ul className="movies__list">
+        {myMovies.map((i) => {
+          return (
+            <Card
+              card={i}
+            />
+          );
+        })}
+      </ul>
     </section>
-  )
+  );
 };
 
 export default SavedMovies;
