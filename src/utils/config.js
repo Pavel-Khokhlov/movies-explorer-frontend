c78535b4e93fc2038ce0674718f90bf41333e56e
Pavel Khokhlov <ya.pavel-kh@yandex.ru>
2021-05-20 08:07:47 +0300

    config update

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
-// export const BASE_URL = "https://api.pavel-khokhlov.nomoredomains.monster";
+export const BASE_URL = "https://api.pavel-khokhlov.nomoredomains.monster";
 
-export const BASE_URL = "http://localhost:3000";
+// export const BASE_URL = "http://localhost:3000";
 
 export const BEATFILM_URL = "https://api.nomoreparties.co";
 export const PATH = "/beatfilm-movies";
